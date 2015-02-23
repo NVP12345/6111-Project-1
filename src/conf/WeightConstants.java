7f58d55b22c211975242fa68907ef6156c4c8552
Nick Pizzoferrato <npizzoferrato@yodle.com>
2015-02-22 20:28:56 -0500

    add weight to title vs. content

--- src/conf/WeightConstants.java
+++ src/conf/WeightConstants.java
@@ -1,6 +1,6 @@
 package conf;
 
-public class ScoringConstants {
+public class WeightConstants {
 
     public static final double TITLE_WEIGHT = .2;
     public static final double CONTENT_WEIGHT = .8;
